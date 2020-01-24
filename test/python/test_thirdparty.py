313e674d205315d173019460a890abfde9ba7494
Paul Landes <landes@mailc.net>
2020-01-24 15:32:05 -0600

    finish stemmer test

--- test/python/test_thirdparty.py
+++ test/python/test_thirdparty.py
@@ -4,7 +4,6 @@ from config import AppConfig
 from zensols.nlp import (
     LanguageResourceFactory,
     TokenNormalizerFactory,
-    PorterStemmer
 )
 
 logger = logging.getLogger(__name__)
@@ -12,22 +11,26 @@ logger = logging.getLogger(__name__)
 
 class TestThirdParty(unittest.TestCase):
     def setUp(self):
-        self.maxDiff = 999999
         self.config = AppConfig('stemmer')
         self.fac = LanguageResourceFactory(self.config)
-        self.lr = self.fac.instance()
 
     def test_stemmer(self):
         tnfac = TokenNormalizerFactory(self.config)
         sent = 'Bobby is fast and runs with dogs, armies, and sheep from the police.'
         lr = self.fac.instance(token_normalizer=tnfac.instance('nonorm'))
-        doc = self.lr.parse(sent)
+        doc = lr.parse(sent)
         feats = tuple(lr.features(doc))
-        print(tuple(map(lambda f: f.norm, feats)))
-        print(tuple(map(lambda f: f.lemma, feats)))
-        # self.assertEqual(('I', 'am', 'a', 'citizen', 'of', 'the United States of America', '.'),
-        #                  tuple(map(lambda f: f.norm, feats)))
+        self.assertEqual(('Bobby', 'is', 'fast', 'and', 'runs', 'with',
+                          'dogs', ',', 'armies', ',', 'and', 'sheep', 'from',
+                          'the', 'police', '.'),
+                         tuple(map(lambda f: f.norm, feats)))
+        self.assertEqual(('Bobby', 'be', 'fast', 'and', 'run', 'with', 'dog',
+                          ',', 'army', ',', 'and', 'sheep', 'from', 'the',
+                          'police', '.'),
+                         tuple(map(lambda f: f.lemma, feats)))
         lr = self.fac.instance(token_normalizer=tnfac.instance('stemmer'))
-        doc = self.lr.parse(sent)
         feats = tuple(lr.features(doc))
-        print(tuple(map(lambda f: f.norm, feats)))
+        self.assertEqual(('bobbi', 'is', 'fast', 'and', 'run', 'with', 'dog',
+                          ',', 'armi', ',', 'and', 'sheep', 'from', 'the',
+                          'polic', '.'),
+                         tuple(map(lambda f: f.norm, feats)))
