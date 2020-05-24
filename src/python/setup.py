4d2f5bf2ff5be8b1eb928193a091fcba2c37dce9
Paul Landes <landes@mailc.net>
2020-05-24 16:56:17 -0500

    doc

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,10 +5,9 @@ su = SetupUtil(
     setup_path=Path(__file__).parent.absolute(),
     name="zensols.nlp",
     package_names=['zensols', 'resources'],
-    # package_data={'': ['*.html', '*.js', '*.css', '*.map', '*.svg']},
-    description='WRITE ME',
+    description='A utility library to assist in parsing natural language text.',
     user='plandes',
     project='nlparse',
     keywords=['tooling'],
-    # has_entry_points=False,
+    has_entry_points=False,
 ).setup()
