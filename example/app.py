b78af519108fb4cdbf5d48085681e27fbdd6d010
Paul Landes <landes@mailc.net>
2021-04-19 08:46:44 -0500

    update example to application

---
+++ example/app.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from zensols.nlp import LanguageResource
+
+
+@dataclass
+class Application(object):
+    """Demonstrate the NLP parsing API.
+
+    """
+    CONFIG_CLI = {'option_includes': {}}
+
+    langres: LanguageResource
+    lc_langres: LanguageResource
+
+    def run(self):
+        sent = 'California is part of the United States.'
+        doc = self.langres.parse(sent)
+        print(type(doc))
+        for tok in doc:
+            print(tok, tok.tag_, tok.is_stop)
+        print('-' * 10)
+        feats = self.langres.features(doc)
+        for feat in feats:
+            print(f'{feat} {type(feat)}')
+            feat.write(depth=1)
+            print('-' * 5)
+            det = feat.detach()
+            print(f'detached: {type(det)}: {det.asdict()}')
+            print('-' * 5)
+        print(', '.join(self.langres.normalized_tokens(doc)))
+        print('-' * 10)
+
+        doc = self.lc_langres.parse(sent)
+        print(', '.join(self.lc_langres.normalized_tokens(doc)))
--- example/parser.py
+++ example/parser.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python
 
-from zensols.config import ExtendedInterpolationEnvConfig, ImportConfigFactory
+from zensols.cli import ApplicationFactory
+from io import StringIO
 
 
-def create_factory():
-    conf = ExtendedInterpolationEnvConfig('parser.conf')
-    return ImportConfigFactory(conf)
+CONFIG = """
+[cli]
+class_name = zensols.cli.ActionCliManager
+apps = list: app
+
+[import]
+files = parser.conf
+
+[app]
+class_name = app.Application
+langres = instance: langres
+lc_langres = instance: lc_langres
+"""
 
 
 def main():
-    sent = 'California is part of the United States.'
-    fac = create_factory()
-    lr = fac('langres')
-    doc = lr.parse(sent)
-    print(type(doc))
-    for tok in doc:
-        print(tok, tok.tag_, tok.is_stop)
-    print('-' * 10)
-    feats = lr.features(doc)
-    for feat in feats:
-        print(f'{feat} {type(feat)}')
-        feat.write(depth=1)
-        print('-' * 5)
-        det = feat.detach()
-        print(f'detached: {type(det)}: {det.to_dict()}')
-        print('-' * 5)
-    print(', '.join(lr.normalized_tokens(doc)))
-    print('-' * 10)
-
-    lr = fac('lc_langres')
-    doc = lr.parse(sent)
-    print(', '.join(lr.normalized_tokens(doc)))
+    print()
+    cli = ApplicationFactory('nlparse', StringIO(CONFIG))
+    cli.invoke()
 
 
 if __name__ == '__main__':
     main()
-
-
-main()
