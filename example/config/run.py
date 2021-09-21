fab1d54fe02f5f1b085c591a80176dfa89c80f38
Paul Landes <landes@mailc.net>
2021-09-21 12:01:51 -0500

    example: move inline config to app.conf

--- example/config/run.py
+++ example/config/run.py
@@ -1,48 +1,10 @@
 #!/usr/bin/env python
 
-from io import StringIO
 from zensols.cli import CliHarness
-
-CONFIG = """
-[cli]
-class_name = zensols.cli.ActionCliManager
-apps = list: log_cli, list_cli, config_cli, app
-
-[log_cli]
-class_name = zensols.cli.LogConfigurator
-log_name = app
-level = debug
-
-[list_cli]
-class_name = zensols.cli.ListActions
-
-[config_cli]
-class_name = zensols.cli.ConfigurationImporter
-# allow overriding the ``zensols.nlp`` resource library
-override = True
-expect = False
-#default = path: default.conf
-
-[import]
-sections = imp_conf
-
-# import the ``zensols.nlp`` library
-[imp_conf]
-type = importini
-config_files = list: resource(zensols.nlp): resources/obj.conf
-
-[app]
-class_name = app.Application
-
-# after we import the ``zensols.nlp`` library (above), we have access to it's
-# configured FeatureDocumentParser with overriden configuration in
-# ``terse.conf``
-doc_parser = instance: doc_parser
-"""
 
 sent = 'Barak Obama was the 44th president of the United States.'
 CliHarness(
-    app_config_resource=StringIO(CONFIG),
+    app_config_resource='app.conf',
     proto_args=['-c', 'terse.conf', sent],
     proto_factory_kwargs={'reload_pattern': '^app'},
 ).run()
