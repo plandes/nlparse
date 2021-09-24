399d6ff379f0ba56d9101b29c3b78f668da29360
Paul Landes <landes@mailc.net>
2021-09-24 18:01:12 -0500

    fix example

--- example/config/run.py
+++ example/config/run.py
@@ -5,6 +5,6 @@ from zensols.cli import CliHarness
 sent = 'Barak Obama was the 44th president of the United States.'
 CliHarness(
     app_config_resource='app.conf',
-    proto_args=['-c', 'terse.conf', sent],
+    proto_args=['parse', '-c', 'terse.conf', sent],
     proto_factory_kwargs={'reload_pattern': '^app'},
 ).run()
