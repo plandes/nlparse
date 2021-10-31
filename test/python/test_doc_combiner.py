18e1f7b0d18a5d8ef2c8e61fd5a65cfbabee5a85
Paul Landes <landes@mailc.net>
2021-10-30 22:53:21 -0500

    more tests

--- test/python/test_doc_combiner.py
+++ test/python/test_doc_combiner.py
@@ -9,13 +9,16 @@ if 0:
 class TestDocCombine(TestBase):
     def test_align(self):
         doc_parser = self.fac.instance('doc_parser_combiner')
-        doc = doc_parser(self.sent_text)
+        doc = doc_parser(self.sent_text2)
         should = \
             ((0, 'I', '<none>'), (2, 'am', '<none>'), (5, 'a', '<none>'),
-             (7, 'citizen', '<none>'), (15, 'of', '<none>'),
-             (18, 'the', 'GPE'), (22, 'United', 'GPE'),
-             (29, 'States', 'GPE'), (36, 'of', 'GPE'),
-             (39, 'America', 'GPE'), (46, '.', '<none>'))
+             (7, 'citizen', '<none>'), (15, 'of', '<none>'), (18, 'the', 'GPE'),
+             (22, 'United', 'GPE'), (29, 'States', 'GPE'), (36, 'of', 'GPE'),
+             (39, 'America', 'GPE'), (46, '.', '<none>'),
+             # sent 2
+             (48, 'My', '<none>'), (51, 'name', '<none>'),
+             (56, 'is', '<none>'), (59, 'Paul', 'PERSON'),
+             (64, 'Landes', 'PERSON'), (70, '.', '<none>'))
         toks = tuple(map(lambda t: (t.idx, t.norm, t.ent_), doc.token_iter()))
         if 0:
             print()
@@ -24,11 +27,14 @@ class TestDocCombine(TestBase):
 
     def test_align_reverse(self):
         doc_parser = self.fac.instance('doc_parser_combiner_reverse')
-        doc = doc_parser(self.sent_text)
+        doc = doc_parser(self.sent_text2)
         should = \
             ((0, 'I', '<none>'), (2, 'am', '<none>'), (5, 'a', '<none>'),
              (7, 'citizen', '<none>'), (15, 'of', '<none>'),
-             (18, 'the United States of America', 'GPE'), (46, '.', '<none>'))
+             (18, 'the United States of America', 'GPE'), (46, '.', '<none>'),
+             # sent 2
+             (48, 'My', '<none>'), (51, 'name', '<none>'), (56, 'is', '<none>'),
+             (59, 'Paul Landes', 'PERSON'), (70, '.', '<none>'))
         toks = tuple(map(lambda t: (t.idx, t.norm, t.ent_), doc.token_iter()))
         if 0:
             print()
