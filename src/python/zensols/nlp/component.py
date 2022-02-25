6a44b4904e6f811e4a0a83b97e311b38a5e7c4f5
Paul Landes <landes@mailc.net>
2022-02-24 20:05:43 -0600

    doc

--- src/python/zensols/nlp/component.py
+++ src/python/zensols/nlp/component.py
@@ -19,12 +19,7 @@ logger = logging.getLogger(__name__)
 
 @Language.component('remove_sent_boundaries')
 def create_remove_sent_boundaries_component(doc: Doc):
-    """Remove sentence boundaries since the corpus already delimits the sentences
-    by newlines.  Otherwise, spaCy will delimit incorrectly as it gets confused
-    with the capitalization in the clickbate "headlines".
-
-    This configuration is used in the ``default.conf`` file's
-    ``remove_sent_boundaries_component`` section.
+    """Remove sentence boundaries from tokens.
 
     :param doc: the spaCy document to remove sentence boundaries
 
