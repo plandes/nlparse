d7969d2efd26ae83cdf950bbce337d86a22fa386
Paul Landes <landes@mailc.net>
2022-08-28 15:34:51 -0500

    fix normalize text write

--- src/python/zensols/nlp/domain.py
+++ src/python/zensols/nlp/domain.py
@@ -150,15 +150,9 @@ class TextContainer(Dictable, metaclass=ABCMeta):
             self._write_line(self.text, depth + 1, writer)
         else:
             if include_original:
-                s = self.text
-                if include_normalized:
-                    s = f'original: {self.text}'
-                self._write_line(s, depth + 1, writer)
+                self._write_line(f'original: {self.text}', depth + 1, writer)
             if include_normalized:
-                s = self.text
-                if include_original:
-                    s = f'normalized: {self.text}'
-                self._write_line(s, depth + 1, writer)
+                self._write_line(f'normalized: {self.norm}', depth + 1, writer)
 
     def __str__(self):
         return f'<{self.norm[:79]}>'
