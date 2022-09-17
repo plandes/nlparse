e3f6a3a38be438cbbaf8f118a16bf2c7dfc7bbcc
Paul Landes <landes@mailc.net>
2022-09-17 15:52:56 -0500

    speed up FeatureToken equals

--- src/python/zensols/nlp/tok.py
+++ src/python/zensols/nlp/tok.py
@@ -244,6 +244,8 @@ class FeatureToken(PersistableContainer, TextContainer):
         self.write_attributes(depth + 2, writer, include_type, feature_ids)
 
     def __eq__(self, other: FeatureToken) -> bool:
+        if id(self) == id(other):
+            return True
         if self.i == other.i and self.idx == other.idx:
             a = dict(self.__dict__)
             b = dict(other.__dict__)
