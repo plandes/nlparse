cf73e85e73c37b7027e20688152cfb9a660801e9
Paul Landes <landes@mailc.net>
2023-07-26 07:50:05 -0500

    set overriden target tokens to NONE that have no source mapping

--- src/python/zensols/nlp/combine.py
+++ src/python/zensols/nlp/combine.py
@@ -101,15 +101,13 @@ class CombinerFeatureDocumentParser(FeatureDocumentParser):
                 src = getattr(source_tok, f)
             else:
                 src = source_tok.get_value(f)
+            src = FeatureToken.NONE if src is None else src
             if logger.isEnabledFor(logging.DEBUG):
-                logger.debug(f'overwrite feature: {f}, src={src}')
-            if src is not None:
-                if logger.isEnabledFor(logging.DEBUG):
-                    logger.debug(f'{src} -> {target_tok.text}.{f}')
-                setattr(target_tok, f, src)
-                if include_detached and \
-                   target_tok._detatched_feature_ids is not None:
-                    target_tok._detatched_feature_ids.add(f)
+                logger.debug(f'overwrite: {src} -> {target_tok.text}.{f}')
+            setattr(target_tok, f, src)
+            if include_detached and \
+               target_tok._detatched_feature_ids is not None:
+                target_tok._detatched_feature_ids.add(f)
 
     def _debug_sentence(self, sent: FeatureSentence, name: str):
         logger.debug(f'{name}:')
@@ -121,9 +119,13 @@ class CombinerFeatureDocumentParser(FeatureDocumentParser):
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'merging sentences: {self._source_sent.tokens} ' +
                          f'-> {self._target_sent.tokens}')
+        n_toks: int = 0
+        assert len(self._target_sent) == len(self._source_sent)
         for target_tok, source_tok in zip(
                 self._target_sent, self._source_sent):
+            assert target_tok.idx == source_tok.idx
             self._merge_tokens(target_tok, source_tok, self._target_sent)
+        assert n_toks == len(self._target_sent)
 
     def _prepare_merge_doc(self):
         pass
@@ -182,9 +184,11 @@ class MappingCombinerFeatureDocumentParser(CombinerFeatureDocumentParser):
     artifacts.
 
     """
-    validate_features: Set[str] = field(default=frozenset({'norm'}))
+    validate_features: Set[str] = field(default=frozenset({'idx'}))
     """A set of features to compare across all tokens when copying.  If any of
-    the given features don't match, an mismatch token error is raised.
+    the given features don't match, an mismatch token error is raised.  The
+    default is the token's index in the document, which should not change in
+    most cases.
 
     """
     merge_sentences: bool = field(default=True)
@@ -195,6 +199,11 @@ class MappingCombinerFeatureDocumentParser(CombinerFeatureDocumentParser):
 
     """
     def _merge_entities_by_token(self, target_tok, source_tok):
+        """Add the source sentence entity spans to the target sentence.  This is
+        important so the original spaCy predicted entities are merged from the
+        source to the target.
+
+        """
         tdoc, sdoc = self._target_doc, self._source_doc
         tsent = None
         try:
@@ -210,24 +219,32 @@ class MappingCombinerFeatureDocumentParser(CombinerFeatureDocumentParser):
                     tsent._ents.append(ent)
 
     def _merge_token_containers(self, target_container: TokenContainer,
-                                rmap: Dict[int, Tuple[FeatureToken, Token]]):
+                                rmap: Dict[int, FeatureToken]):
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'merge: {target_container}, mapping: {rmap}')
+        visited: Set[FeatureToken] = set()
+        targ_toks: Set[FeatureToken] = set(target_container.token_iter())
         for target_tok in target_container.token_iter():
-            source_tok = rmap.get(target_tok.idx)
+            source_tok: FeatureToken = rmap.get(target_tok.idx)
             if logger.isEnabledFor(logging.DEBUG):
                 logger.debug(f'entry: {target_tok.idx}/{target_tok} ' +
                              f'-> {source_tok}')
             if source_tok is not None:
+                visited.add(source_tok)
                 self._merge_tokens(target_tok, source_tok, target_container)
                 if source_tok.ent_ != FeatureToken.NONE:
                     self._merge_entities_by_token(target_tok, source_tok)
+        for target_tok in (targ_toks - visited):
+            fname: str
+            for fname in self.overwrite_features:
+                if not hasattr(target_tok, fname):
+                    setattr(target_tok, fname, FeatureToken.NONE)
 
     def _merge_sentence(self):
         if logger.isEnabledFor(logging.DEBUG):
             logger.debug(f'merging sentences: {self._source_sent.tokens} ' +
                          f'-> {self._target_sent.tokens}')
-        rmp: Dict[int, Tuple[FeatureToken, Token]] = self._source_token_mapping
+        rmp: Dict[int, FeatureToken] = self._source_token_mapping
         self._merge_token_containers(self._target_sent, rmp)
         self._target_doc._combine_update(self._source_doc)
 
